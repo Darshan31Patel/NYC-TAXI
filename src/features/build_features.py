a5a99496aa87c2bceea96e61cccce580f44ec653
Darshan31Patel <darshan.h3112@gmail.com>
2024-05-30 17:26:44 +0530

    data preprocessed

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,8 +1,20 @@
 import sys
+import logging
 import pandas as pd
 import numpy as np
 from pathlib import Path
 from distances import haversine_distance,manhattan_distance,euclidean_distance
+from src.logger import create_log_path, CustomLogger
+
+# path to save the log files
+log_file_path = create_log_path('build_features')
+
+# create the custom logger object
+build_features = CustomLogger(logger_name='build_features',
+                              log_filename=log_file_path)
+# set the level of logging to INFO
+build_features.set_log_level(level=logging.INFO)
+
 
 new_features = ['haversine_distance','euclidean_distance','manhattan_distance']
 
@@ -13,7 +25,8 @@ def implement_distances(dataframe:pd.DataFrame, lat1:pd.Series,lon1:pd.Series,la
     for ind in range(len(build_features_list)):
         func = build_features_list[ind]
         dataframe[new_features[ind]] = func(lat1,lon1,lat2,lon2)
-    
+
+        build_features.save_logs(msg=f'Feature {build_features_list[ind]} created and added to dataframe')
     return dataframe
 
 
@@ -44,3 +57,7 @@ if __name__=="__main__":
         output_path = root_path/"data/processed/build-features"
         output_path.mkdir(exist_ok=True,parents=True)
         save_dataframe(df,output_path/filename)
+
+        build_features.save_logs(msg=f'Dataframe saved at {output_path/filename}')
+        
+    build_features.save_logs(msg="Different distance columns added successfully")
