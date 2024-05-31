30cfaa15d7501c7378a02e8b164d8989a125f8eb
Darshan31Patel <darshan.h3112@gmail.com>
2024-05-31 15:27:58 +0530

    mlflow added

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,55 +1,126 @@
+# import sys
+# import joblib
+# import pandas as pd
+# from yaml import safe_load
+# from pathlib import Path
+# from sklearn.ensemble import RandomForestRegressor
+
+# TARGET = 'trip_duration'
+
+
+# def load_dataframe(path):
+#     df = pd.read_csv(path)
+#     return df
+
+
+# def make_X_y(dataframe:pd.DataFrame, target_column:str):
+#     df_copy =  dataframe.copy()
+#     X = df_copy.drop(columns=target_column)
+#     y = df_copy[target_column]
+
+#     return X,y
+
+
+# def train_model(model,X_train,y_train):
+#     model.fit(X_train,y_train)
+#     return model
+
+
+# def save_model(model,save_path):
+#     joblib.dump(model,save_path)
+
+
+# def main():
+#     current_path = Path(__file__)
+#     root_path = current_path.parent.parent.parent
+#     training_data_path = root_path/sys.argv[1]
+#     train_data = load_dataframe(training_data_path)
+#     X_train,y_train = make_X_y(dataframe=train_data,target_column=TARGET)
+
+#     with open("params.yaml") as f:
+#         params = safe_load(f)
+
+#     model_params = params['train_model']['random_forest_regressor']
+
+#     regressor = RandomForestRegressor(**model_params)
+#     regressor = train_model(model=regressor,X_train=X_train,y_train=y_train)
+
+#     model_output_path = root_path/'models'/'models'
+#     model_output_path.mkdir(exist_ok=True)
+
+#     save_model(model=regressor, save_path=model_output_path/'rf.joblib')
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
 import sys
 import joblib
 import pandas as pd
 from yaml import safe_load
 from pathlib import Path
 from sklearn.ensemble import RandomForestRegressor
+import mlflow
+import mlflow.sklearn
 
 TARGET = 'trip_duration'
-
 
 def load_dataframe(path):
     df = pd.read_csv(path)
     return df
 
-
-def make_X_y(dataframe:pd.DataFrame, target_column:str):
-    df_copy =  dataframe.copy()
+def make_X_y(dataframe: pd.DataFrame, target_column: str):
+    df_copy = dataframe.copy()
     X = df_copy.drop(columns=target_column)
     y = df_copy[target_column]
+    return X, y
 
-    return X,y
-
-
-def train_model(model,X_train,y_train):
-    model.fit(X_train,y_train)
+def train_model(model, X_train, y_train):
+    model.fit(X_train, y_train)
     return model
 
-
-def save_model(model,save_path):
-    joblib.dump(model,save_path)
-
+def save_model(model, save_path):
+    joblib.dump(model, save_path)
 
 def main():
     current_path = Path(__file__)
     root_path = current_path.parent.parent.parent
-    training_data_path = root_path/sys.argv[1]
+    training_data_path = root_path / sys.argv[1]
     train_data = load_dataframe(training_data_path)
-    X_train,y_train = make_X_y(dataframe=train_data,target_column=TARGET)
+    X_train, y_train = make_X_y(dataframe=train_data, target_column=TARGET)
 
     with open("params.yaml") as f:
         params = safe_load(f)
 
     model_params = params['train_model']['random_forest_regressor']
 
-    regressor = RandomForestRegressor(**model_params)
-    regressor = train_model(model=regressor,X_train=X_train,y_train=y_train)
+    # Start MLflow run
+    with mlflow.start_run():
+        # Log parameters
+        mlflow.log_params(model_params)
 
-    model_output_path = root_path/'models'/'models'
-    model_output_path.mkdir(exist_ok=True)
+        regressor = RandomForestRegressor(**model_params)
+        regressor = train_model(model=regressor, X_train=X_train, y_train=y_train)
 
-    save_model(model=regressor, save_path=model_output_path/'rf.joblib')
+        model_output_path = root_path / 'models' / 'models'
+        model_output_path.mkdir(exist_ok=True)
 
+        model_save_path = model_output_path / 'rf.joblib'
+        save_model(model=regressor, save_path=model_save_path)
+
+        # Log model
+        mlflow.sklearn.log_model(regressor, "random_forest_model")
+
+        # Log the training data path
+        mlflow.log_param("training_data_path", training_data_path)
+
+        # If there are metrics to log, you can log them here
+        # e.g., mlflow.log_metric("example_metric", example_metric_value)
+
+        # Log the model file as an artifact
+        mlflow.log_artifact(model_save_path)
 
 if __name__ == "__main__":
     main()
