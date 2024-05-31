f0d253de811884e11c0dd3d7436368094be5adfc
Darshan31Patel <darshan.h3112@gmail.com>
2024-05-31 16:26:51 +0530

    mlflow code added

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,64 +1,10 @@
-# import sys
-# import joblib
-# import pandas as pd
-# from pathlib import Path
-# from sklearn.metrics import r2_score
-
-# TARGET = 'trip_duration'
-# MODEL_NAME = 'rf.joblib'
-
-
-# def load_dataframe(path):
-#     df = pd.read_csv(path)
-#     return df
-    
-    
-# def make_X_y(dataframe:pd.DataFrame,target_column:str):
-#     df_copy = dataframe.copy()
-#     X = df_copy.drop(columns=target_column,axis=1)
-#     y = df_copy[target_column]
-    
-#     return X, y
-
-
-# def get_predictions(model, X:pd.DataFrame):
-#     y_pred = model.predict(X)
-#     return y_pred
-
-
-# def calculate_r2_score(y_true,y_pred):
-#     score = r2_score(y_true=y_true,y_pred=y_pred)
-#     return score
-
-
-# def main():
-#     current_path = Path(__file__)
-#     root_path = current_path.parent.parent.parent
-
-#     for ind in range(1,3):
-#         data_path = root_path/'data/processed/final'/sys.argv[ind]
-#         data = load_dataframe(data_path)
-#         X_test,y_test = make_X_y(dataframe=data,target_column=TARGET)
-#         model_path = root_path/'models'/'models'/MODEL_NAME
-
-#         model = joblib.load(model_path)
-#         y_pred = get_predictions(model=model,X=X_test)
-#         score = calculate_r2_score(y_true=y_test,y_pred=y_pred)
-
-#         print(f"\n The score for dataset {sys.argv[ind]} is {score}")
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 import sys
 import joblib
 import pandas as pd
 from pathlib import Path
 from sklearn.metrics import r2_score
 import mlflow
-import mlflow.sklearn
+import mlflow.sklearn   
 
 TARGET = 'trip_duration'
 MODEL_NAME = 'rf.joblib'
@@ -81,16 +27,6 @@ def calculate_r2_score(y_true, y_pred):
     score = r2_score(y_true=y_true, y_pred=y_pred)
     return score
 
-def log_mlflow(params, model, model_name, data_path=None, score=None):
-    with mlflow.start_run():
-        if params:
-            mlflow.log_params(params)
-        if data_path:
-            mlflow.log_param("data_path", str(data_path))
-        if score is not None:
-            mlflow.log_metric("r2_score", score)
-        if model:
-            mlflow.sklearn.log_model(model, model_name)
 
 def main():
     current_path = Path(__file__)
@@ -108,14 +44,11 @@ def main():
 
         print(f"\nThe score for dataset {sys.argv[ind]} is {score}")
 
-        # Use the MLflow logging function
-        log_mlflow(
-            params=None, 
-            model=model, 
-            model_name="model", 
-            data_path=data_path, 
-            score=score
-        )
+        with mlflow.start_run():
+            mlflow.log_params(model.get_params())
+            mlflow.log_param("data_path", str(data_path))
+            mlflow.log_metric("r2_score", score)
+            mlflow.sklearn.log_model(model, "model")
 
 if __name__ == "__main__":
     main()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,106 +1,41 @@
-# import sys
-# import joblib
-# import pandas as pd
-# from yaml import safe_load
-# from pathlib import Path
-# from sklearn.ensemble import RandomForestRegressor
-
-# TARGET = 'trip_duration'
-
-
-# def load_dataframe(path):
-#     df = pd.read_csv(path)
-#     return df
-
-
-# def make_X_y(dataframe:pd.DataFrame, target_column:str):
-#     df_copy =  dataframe.copy()
-#     X = df_copy.drop(columns=target_column)
-#     y = df_copy[target_column]
-
-#     return X,y
-
-
-# def train_model(model,X_train,y_train):
-#     model.fit(X_train,y_train)
-#     return model
-
-
-# def save_model(model,save_path):
-#     joblib.dump(model,save_path)
-
-
-# def main():
-#     current_path = Path(__file__)
-#     root_path = current_path.parent.parent.parent
-#     training_data_path = root_path/sys.argv[1]
-#     train_data = load_dataframe(training_data_path)
-#     X_train,y_train = make_X_y(dataframe=train_data,target_column=TARGET)
-
-#     with open("params.yaml") as f:
-#         params = safe_load(f)
-
-#     model_params = params['train_model']['random_forest_regressor']
-
-#     regressor = RandomForestRegressor(**model_params)
-#     regressor = train_model(model=regressor,X_train=X_train,y_train=y_train)
-
-#     model_output_path = root_path/'models'/'models'
-#     model_output_path.mkdir(exist_ok=True)
-
-#     save_model(model=regressor, save_path=model_output_path/'rf.joblib')
-
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 import sys
 import joblib
 import pandas as pd
 from yaml import safe_load
 from pathlib import Path
 from sklearn.ensemble import RandomForestRegressor
-import mlflow
-import mlflow.sklearn
 
 TARGET = 'trip_duration'
+
 
 def load_dataframe(path):
     df = pd.read_csv(path)
     return df
 
-def make_X_y(dataframe: pd.DataFrame, target_column: str):
-    df_copy = dataframe.copy()
+
+def make_X_y(dataframe:pd.DataFrame, target_column:str):
+    df_copy =  dataframe.copy()
     X = df_copy.drop(columns=target_column)
     y = df_copy[target_column]
-    return X, y
 
-def train_model(model, X_train, y_train):
-    model.fit(X_train, y_train)
+    return X,y
+
+
+def train_model(model,X_train,y_train):
+    model.fit(X_train,y_train)
     return model
 
-def save_model(model, save_path):
-    joblib.dump(model, save_path)
 
-def log_mlflow(params, model, model_name, data_path=None, score=None):
-    with mlflow.start_run():
-        if params:
-            mlflow.log_params(params)
-        if data_path:
-            mlflow.log_param("data_path", str(data_path))
-        if score is not None:
-            mlflow.log_metric("r2_score", score)
-        if model:
-            mlflow.sklearn.log_model(model, model_name)
+def save_model(model,save_path):
+    joblib.dump(model,save_path)
+
 
 def main():
     current_path = Path(__file__)
     root_path = current_path.parent.parent.parent
-    training_data_path = root_path / sys.argv[1]
+    training_data_path = root_path/sys.argv[1]
     train_data = load_dataframe(training_data_path)
-    X_train, y_train = make_X_y(dataframe=train_data, target_column=TARGET)
+    X_train,y_train = make_X_y(dataframe=train_data,target_column=TARGET)
 
     with open("params.yaml") as f:
         params = safe_load(f)
@@ -108,21 +43,13 @@ def main():
     model_params = params['train_model']['random_forest_regressor']
 
     regressor = RandomForestRegressor(**model_params)
-    regressor = train_model(model=regressor, X_train=X_train, y_train=y_train)
+    regressor = train_model(model=regressor,X_train=X_train,y_train=y_train)
 
-    model_output_path = root_path / 'models' / 'models'
+    model_output_path = root_path/'models'/'models'
     model_output_path.mkdir(exist_ok=True)
 
-    model_save_path = model_output_path / 'rf.joblib'
-    save_model(model=regressor, save_path=model_save_path)
+    save_model(model=regressor, save_path=model_output_path/'rf.joblib')
 
-    # Use the MLflow logging function
-    log_mlflow(
-        params=model_params, 
-        model=regressor, 
-        model_name="random_forest_model", 
-        data_path=training_data_path
-    )
 
 if __name__ == "__main__":
     main()
